6669170170ee9e836b53c7b8762996bf92d1bf95
Rafa Zaidan <rafayzaidanapple@gmail.com>
2020-08-02 17:35:05 +0200

    Build the UI for the category view controller

---
+++ Coder-swag/Coder-swag/View/CategoryCell.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryCell.swift
+//  Coder-swag
+//
+//  Created by Rafa Zaidan on 02/08/2020.
+//  Copyright © 2020 Rafa Zaidan. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryTitle: UILabel!
+    @IBOutlet weak var categoryImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
